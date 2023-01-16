9bc3515e8953e377b80a7397aeb12c8e63a0aa7d
Yagnik Kantariya <yagnik@logicwind.co>
2023-01-16 16:18:52 +0530

    Squashed commit of the following:
    commit bf3613fbd4af6594a5770502e98dde89c64eeb7f
    Author: Yagnik Kantariya <yagnik@logicwind.co>
    Date:   Mon Jan 16 16:00:57 2023 +0530
    
        --example change
    
    commit a9c3fdc688a41f4265dc542685bb740f19c764a7
    Author: Yagnik Kantariya <yagnik@logicwind.co>
    Date:   Mon Jan 16 15:41:52 2023 +0530
    
        - rename package name
        - make changes on ios native side and ios side
    
    commit 53087410b46fb2b027d5d9a20e8c091df72b71b1
    Author: Bhavesh <bhavesh@Bhaveshs-MacBook-Pro.local>
    Date:   Mon Jan 16 13:37:22 2023 +0530
    
        Added package name and description

--- ios/Classes/FlutterAppUpdatePackagePlugin.m
+++ ios/Classes/FlutterAppUpdatePackagePlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterAppUpdatePackagePlugin.h"
-#if __has_include(<flutter_app_update_package/flutter_app_update_package-Swift.h>)
-#import <flutter_app_update_package/flutter_app_update_package-Swift.h>
+#if __has_include(<appsonair_flutter_sdk/appsonair_flutter_sdk-Swift.h>)
+#import <appsonair_flutter_sdk/appsonair_flutter_sdk-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_app_update_package-Swift.h"
+#import "appsonair_flutter_sdk-Swift.h"
 #endif
 
 @implementation FlutterAppUpdatePackagePlugin
